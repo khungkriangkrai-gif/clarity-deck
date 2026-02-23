386cb27988e46c0bd46ae03b3956637480e8bd76
khungkriangkrai-gif <khung.kriangkrai@gmail.com>
2026-02-23 09:05:50 +0700

    Create use_cases.py

---
+++ application/use_cases.py
@@ -0,0 +1,20 @@
+from domain.tarot_engine import TarotEngine
+from domain.models import Reading
+
+class RunReadingUseCase:
+
+    def __init__(self, deck_repo, gpt_client):
+        self.deck_repo = deck_repo
+        self.engine = TarotEngine()
+        self.gpt = gpt_client
+
+    def execute(self, context, num_cards):
+
+        deck = self.deck_repo.load_deck()
+        cards = self.engine.draw(deck, num_cards)
+
+        reading = Reading(context=context, cards=cards)
+
+        ai_analysis = self.gpt.analyze(context, cards)
+
+        return reading, ai_analysis
